cccea575ca3c45d8af64aa2a1fe150163a9abbb5
Balla Kavya <ballakavya594@gmail.com>
2024-07-12 11:23:07 +0530

    "Updated Navbar CSS and JS: added width to navbar, updated brand margin, added custom toggler styles, and modified navbar collapse behavior for different screen sizes."

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './component/Home/Home.js';
 import About from './component/About/About.js';
-import Navbar from './component/Navbar/Navbar.js';
+import CustomNavbar from './component/Navbar/Navbar.js';
 import Skills from './component/Skills/Skills';
 import Project from './component/Projects/Projects.js';
 import StarsCanvas from './component/StarCanvas.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
- 
   return (
     <div>
       <StarsCanvas />
-      <Navbar />
+      <CustomNavbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
